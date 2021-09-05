b7e1d52053efaa06130369788b8b22a2c8d62fe4
komal1802shrivastava <90084003+komal1802shrivastava@users.noreply.github.com>
2021-09-06 01:23:13 +0530

    Tipcalculator folder added

---
+++ TipCalculator2/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace TipCalculator2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BillL = new System.Windows.Forms.Label();
+            this.billBoxField = new System.Windows.Forms.TextBox();
+            this.TipL = new System.Windows.Forms.Label();
+            this.tipBoxField = new System.Windows.Forms.TextBox();
+            this.NoofpeopleL = new System.Windows.Forms.Label();
+            this.noOfPeopleField = new System.Windows.Forms.TextBox();
+            this.PerpersonLL = new System.Windows.Forms.Label();
+            this.TipLL = new System.Windows.Forms.Label();
+            this.TotalL = new System.Windows.Forms.Label();
+            this.PerpersonL = new System.Windows.Forms.Label();
+            this.Incrementnoofpeople = new System.Windows.Forms.Button();
+            this.Decrementnoofpeople = new System.Windows.Forms.Button();
+            this.Incrementtip = new System.Windows.Forms.Button();
+            this.Decrementtip = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.Submitbutton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BillL
+            // 
+            this.BillL.AutoSize = true;
+            this.BillL.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BillL.Location = new System.Drawing.Point(104, 59);
+            this.BillL.Name = "BillL";
+            this.BillL.Size = new System.Drawing.Size(56, 25);
+            this.BillL.TabIndex = 0;
+            this.BillL.Text = "BILL";
+            // 
+            // billBoxField
+            // 
+            this.billBoxField.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.billBoxField.Location = new System.Drawing.Point(23, 87);
+            this.billBoxField.Name = "billBoxField";
+            this.billBoxField.Size = new System.Drawing.Size(250, 30);
+            this.billBoxField.TabIndex = 1;
+            this.billBoxField.TextChanged += new System.EventHandler(this.billBox);
+            // 
+            // TipL
+            // 
+            this.TipL.AutoSize = true;
+            this.TipL.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TipL.Location = new System.Drawing.Point(104, 139);
+            this.TipL.Name = "TipL";
+            this.TipL.Size = new System.Drawing.Size(77, 25);
+            this.TipL.TabIndex = 2;
+            this.TipL.Text = " TIP %";
+            // 
+            // tipBoxField
+            // 
+            this.tipBoxField.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tipBoxField.Location = new System.Drawing.Point(94, 176);
+            this.tipBoxField.Name = "tipBoxField";
+            this.tipBoxField.Size = new System.Drawing.Size(120, 30);
+            this.tipBoxField.TabIndex = 3;
+            this.tipBoxField.TextChanged += new System.EventHandler(this.tipBox);
+            // 
+            // NoofpeopleL
+            // 
+            this.NoofpeopleL.AutoSize = true;
+            this.NoofpeopleL.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NoofpeopleL.Location = new System.Drawing.Point(104, 228);
+            this.NoofpeopleL.Name = "NoofpeopleL";
+            this.NoofpeopleL.Size = new System.Drawing.Size(153, 25);
+            this.NoofpeopleL.TabIndex = 4;
+            this.NoofpeopleL.Text = " No. Of People";
+            // 
+            // noOfPeopleField
+            // 
+            this.noOfPeopleField.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.noOfPeopleField.Location = new System.Drawing.Point(94, 265);
+            this.noOfPeopleField.Name = "noOfPeopleField";
+            this.noOfPeopleField.Size = new System.Drawing.Size(120, 30);
+            this.noOfPeopleField.TabIndex = 5;
+            this.noOfPeopleField.TextChanged += new System.EventHandler(this.noOfPeopleBox);
+            // 
+            // PerpersonLL
+            // 
+            this.PerpersonLL.Location = new System.Drawing.Point(0, 0);
+            this.PerpersonLL.Name = "PerpersonLL";
+            this.PerpersonLL.Size = new System.Drawing.Size(100, 23);
+            this.PerpersonLL.TabIndex = 20;
+            // 
+            // TipLL
+            // 
+            this.TipLL.AutoSize = true;
+            this.TipLL.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TipLL.Location = new System.Drawing.Point(427, 90);
+            this.TipLL.Name = "TipLL";
+            this.TipLL.Size = new System.Drawing.Size(65, 38);
+            this.TipLL.TabIndex = 7;
+            this.TipLL.Text = "Tip";
+            // 
+            // TotalL
+            // 
+            this.TotalL.AutoSize = true;
+            this.TotalL.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TotalL.Location = new System.Drawing.Point(427, 228);
+            this.TotalL.Name = "TotalL";
+            this.TotalL.Size = new System.Drawing.Size(94, 38);
+            this.TotalL.TabIndex = 8;
+            this.TotalL.Text = "Total";
+            // 
+            // PerpersonL
+            // 
+            this.PerpersonL.AutoSize = true;
+            this.PerpersonL.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PerpersonL.Location = new System.Drawing.Point(429, 265);
+            this.PerpersonL.Name = "PerpersonL";
+            this.PerpersonL.Size = new System.Drawing.Size(123, 25);
+            this.PerpersonL.TabIndex = 9;
+            this.PerpersonL.Text = " Per person";
+            // 
+            // Incrementnoofpeople
+            // 
+            this.Incrementnoofpeople.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Incrementnoofpeople.Location = new System.Drawing.Point(213, 265);
+            this.Incrementnoofpeople.Name = "Incrementnoofpeople";
+            this.Incrementnoofpeople.Size = new System.Drawing.Size(60, 30);
+            this.Incrementnoofpeople.TabIndex = 13;
+            this.Incrementnoofpeople.Text = "+";
+            this.Incrementnoofpeople.UseVisualStyleBackColor = true;
+            this.Incrementnoofpeople.Click += new System.EventHandler(this.incrementNoOfPeople);
+            // 
+            // Decrementnoofpeople
+            // 
+            this.Decrementnoofpeople.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Decrementnoofpeople.Location = new System.Drawing.Point(38, 265);
+            this.Decrementnoofpeople.Name = "Decrementnoofpeople";
+            this.Decrementnoofpeople.Size = new System.Drawing.Size(60, 30);
+            this.Decrementnoofpeople.TabIndex = 14;
+            this.Decrementnoofpeople.Text = "-";
+            this.Decrementnoofpeople.UseVisualStyleBackColor = true;
+            this.Decrementnoofpeople.Click += new System.EventHandler(this.decrementNoOfPeople);
+            // 
+            // Incrementtip
+            // 
+            this.Incrementtip.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Incrementtip.Location = new System.Drawing.Point(213, 176);
+            this.Incrementtip.Name = "Incrementtip";
+            this.Incrementtip.Size = new System.Drawing.Size(60, 30);
+            this.Incrementtip.TabIndex = 15;
+            this.Incrementtip.Text = "+";
+            this.Incrementtip.UseVisualStyleBackColor = true;
+            this.Incrementtip.Click += new System.EventHandler(this.incrementTip);
+            // 
+            // Decrementtip
+            // 
+            this.Decrementtip.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Decrementtip.Location = new System.Drawing.Point(38, 176);
+            this.Decrementtip.Name = "Decrementtip";
+            this.Decrementtip.Size = new System.Drawing.Size(60, 30);
+            this.Decrementtip.TabIndex = 16;
+            this.Decrementtip.Text = "-";
+            this.Decrementtip.UseVisualStyleBackColor = true;
+            this.Decrementtip.Click += new System.EventHandler(this.decrementTip);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 38;
+            this.listBox1.Location = new System.Drawing.Point(582, 87);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(188, 80);
+            this.listBox1.TabIndex = 17;
+            // 
+            // listBox2
+            // 
+            this.listBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 38;
+            this.listBox2.Location = new System.Drawing.Point(582, 216);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(188, 80);
+            this.listBox2.TabIndex = 18;
+            // 
+            // Submitbutton
+            // 
+            this.Submitbutton.Location = new System.Drawing.Point(330, 352);
+            this.Submitbutton.Name = "Submitbutton";
+            this.Submitbutton.Size = new System.Drawing.Size(121, 47);
+            this.Submitbutton.TabIndex = 19;
+            this.Submitbutton.Text = "Submit";
+            this.Submitbutton.UseVisualStyleBackColor = true;
+            this.Submitbutton.Click += new System.EventHandler(this.submitButton);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(435, 128);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 25);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "Per person";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Submitbutton);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.Decrementtip);
+            this.Controls.Add(this.Incrementtip);
+            this.Controls.Add(this.Decrementnoofpeople);
+            this.Controls.Add(this.Incrementnoofpeople);
+            this.Controls.Add(this.PerpersonL);
+            this.Controls.Add(this.TotalL);
+            this.Controls.Add(this.TipLL);
+            this.Controls.Add(this.PerpersonLL);
+            this.Controls.Add(this.noOfPeopleField);
+            this.Controls.Add(this.NoofpeopleL);
+            this.Controls.Add(this.tipBoxField);
+            this.Controls.Add(this.TipL);
+            this.Controls.Add(this.billBoxField);
+            this.Controls.Add(this.BillL);
+            this.Name = "Form1";
+            this.Text = "TIP CALCULATOR";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label BillL;
+        private System.Windows.Forms.TextBox billBoxField;
+        private System.Windows.Forms.Label TipL;
+        private System.Windows.Forms.TextBox tipBoxField;
+        private System.Windows.Forms.Label NoofpeopleL;
+        private System.Windows.Forms.TextBox noOfPeopleField;
+        private System.Windows.Forms.Label PerpersonLL;
+        private System.Windows.Forms.Label TipLL;
+        private System.Windows.Forms.Label TotalL;
+        private System.Windows.Forms.Label PerpersonL;
+        private System.Windows.Forms.Button Incrementnoofpeople;
+        private System.Windows.Forms.Button Decrementnoofpeople;
+        private System.Windows.Forms.Button Incrementtip;
+        private System.Windows.Forms.Button Decrementtip;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button Submitbutton;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TipCalculator2/Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TipCalculator2
+{
+   
+    public partial class Form1 : Form
+    {
+        //ERROR HANDLING CLASS
+        class MyException : ApplicationException
+        {
+            //ERROR HANDLING FUNCTION WHEN USERS ENTER NEGATIVE NUMBER IN THE TIP FIELD .
+            public void TipNegativeException()
+            {
+                MessageBox.Show("TIP SHOULD BE IN POSITIVE");
+            }
+
+            //ERROR HANDLING FUNCTION WHEN USERS ENTER ZERO OR NEGATIVE NUMBER IN THE NO. OF PEOPLE FIELD .
+            public void PeopleNegativeZeroException()
+            {
+                MessageBox.Show("N0. OF PEOPLE SHOULD BE GREATER THAN ZERO");
+            }
+
+            //ERROR HANDLING FUNCTION WHEN USERS ENTER NEGATIVE NUMBER IN THE COSTOFMEAL FIELD .
+            public void BillNegativeException()
+            {
+                MessageBox.Show("BILL SHOULD BE IN POSITIVE");
+            }
+
+            //ERROR HANDLING FUNCTION WHEN SOME UNEXPECTED SITUATION HAPPENS.
+            public void UnexpectedError()
+            {
+                MessageBox.Show("SOMETHING IS WRONG !!! ");
+            }
+
+            //ERROR HANDLING FUNCTION WHEN NO. OF PEOPLE FIELDS NOT IS IN INTEGER.
+            public void PeopleInteger()
+            {
+                MessageBox.Show("NUMBER OF PEOPLE SHOULD BE IN INTEGER !!! ");
+            }
+
+            //ERROR HANDLING FUNCTION WHEN USERS FORGET TO ENTER SOME DETAILS.
+            public void FieldEmpty()
+            {
+                MessageBox.Show("FIELD IS EMPTY!!! ");
+            }
+
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void billBox(object sender, EventArgs e)    //COSTOFMEAL TEXTBOX.
+
+        {
+            try
+            {
+                //STATEMENT FOR ENTERING DETAILS IN COSTOFMEAL TEXTBOX
+                double bill = Convert.ToDouble(this.billBoxField.Text);
+            }
+
+            catch (Exception)
+            {
+                //POPUP WHEN USER TRIES TO ENTER STRING IN COSTOFMEAL FIELDS.
+                MessageBox.Show("BILL SHOULD BE IN NUMERIC VALUE !!! ");
+            }
+        }
+
+        private void noOfPeopleBox(object sender, EventArgs e)     //No. of people textbox
+        {
+            try
+            {
+                //STATEMENT FOR ENTERING DETAILS IN NO. OF PEOPLE TEXTBOX.
+                int noOfPeople = Convert.ToInt32(noOfPeopleField.Text);
+            }
+
+            catch (Exception)
+            {
+                //POPUP WHEN USER TRIES TO ENTER SOMETHING RATHER THAN INTEGER IN NO. OF PEOPLE FIELD .
+                MessageBox.Show("NUMBER OF PEOPLE SHOULD BE INTEGER !!! ");
+            }
+
+        }
+
+
+        //BUTTON WHICH DECREMENT THE TIP VALUE BY 1
+        private void decrementTip(object sender, EventArgs e)
+        {
+            double tip;
+            tip = Convert.ToDouble(tipBoxField.Text);
+            tip = tip - 1;
+            tipBoxField.Text = tip.ToString();
+
+        }
+
+
+        //BUTTON WHICH INCREMENT THE NO. OF PEOPLE VALUE BY 1
+        private void incrementNoOfPeople(object sender, EventArgs e)
+        {
+            double noOfPeople;
+            noOfPeople = Convert.ToDouble(noOfPeopleField.Text);
+            noOfPeople = noOfPeople + 1;
+            noOfPeopleField.Text = noOfPeople.ToString();
+
+        }
+
+
+        //BUTTON WHICH DECREMENT THE NO. OF PEOPLE VALUE BY 1
+        private void decrementNoOfPeople(object sender, EventArgs e)
+        {
+            double noOfPeople;          // NO. OF PEOPLE
+            noOfPeople = Convert.ToDouble(noOfPeopleField.Text);
+            noOfPeople = noOfPeople - 1;
+            noOfPeopleField.Text = noOfPeople.ToString();
+
+        }
+
+
+        //BUTTON WHICH INCREMENT THE TIP VALUE BY 1
+        private void incrementTip(object sender, EventArgs e)
+        {
+            double tip;         //TIP
+            tip = Convert.ToDouble(tipBoxField.Text);
+            tip = tip + 1;
+            tipBoxField.Text = tip.ToString();
+
+        }
+
+        private void tipBox(object sender, EventArgs e)
+        {
+            try
+            {
+                double tip = Convert.ToDouble(tipBoxField.Text);     //STATEMENT FOR ENTERING DETAILS IN TIP TEXTBOX.
+            }
+
+            catch (Exception)
+            {
+                MessageBox.Show("TIP SHOULD BE IN NUMERIC VALUE!!! ");    //POPUP WHEN USER TRIES TO ENTER STRING IN TIP FIELD .
+            }
+        }
+
+        //BUTTON TO SUBMIT THE DETAILS IN THE FORM.
+        private void submitButton(object sender, EventArgs e)
+        {
+            double costOfMeal;          // COST OF THE MEAL
+            double tip;                  // TIP 
+            int noOfPeople;              // NO. OF PEOPLE 
+            double tipPerPerson;        // TIP PER PERSON
+            double billPerPerson;       // BILL PER PERSON
+            try
+            {
+                try
+                {
+                    //WHEN USER FORGETS TO PUT DETAILS IT WILL THROW AN ERROR
+                    if (String.IsNullOrEmpty(billBoxField.Text) || String.IsNullOrEmpty(tipBoxField.Text) || String.IsNullOrEmpty(noOfPeopleField.Text))
+                    {
+                        throw new MyException();
+                    }
+                }
+
+                catch(MyException ex6)
+                {
+                    ex6.FieldEmpty();
+
+                }
+
+                //INPUT OF THE COST OF MEAL TAKEN FROM THE USER.
+                costOfMeal = Convert.ToDouble(billBoxField.Text);
+
+                //INPUT OF THE TIP TAKEN FROM THE USER.
+                tip = Convert.ToDouble(tipBoxField.Text);
+
+                //INPUT OF THE NO. OF PEOPLE TAKEN FROM THE USER.
+                noOfPeople = Convert.ToInt32(noOfPeopleField.Text);
+
+                try
+                {
+                    //WHEN COST OF MEAL LESS THAN ZERO IT WILL THROW AN ERROR.
+                    if (costOfMeal < 0)
+                    {
+                        throw new MyException();
+                    }
+                }
+
+                catch(MyException ex1)
+                {
+                    //CALLING OF THE FUNCTION BillNegativeException
+                    ex1.BillNegativeException();
+                }
+
+                try
+                {
+                    //WHEN TIP IS LESS  THAN ZERO IT WILL THROW AN ERROR. 
+                    if (tip < 0)
+                    {
+                        throw new MyException();
+                    }
+                }
+
+                catch(MyException ex2)
+                {
+                    //CALLING OF THE FUNCTION TipNegativeException.
+                    ex2.TipNegativeException();
+                }
+                try
+                {
+                    //WHEN NO. OF PEOPLE IS LESS THAN OR EQUAL TO ZERO IT WILL THROW AN ERROR.
+                    if(noOfPeople <= 0)
+                    {
+                        throw new MyException();
+                    }
+                }
+
+                catch(MyException ex3)
+                {
+                    //CALLING OF THE FUNCTION .PeopleNegativeZeroException.
+                    ex3.PeopleNegativeZeroException();
+                }
+
+
+                tipPerPerson = (costOfMeal * (tip / 100)) / noOfPeople;         //FORMULA FOR TIP PER PERSON
+                billPerPerson = (costOfMeal / noOfPeople) + tipPerPerson;       //FORMULA FOR TOTAL BILL PER PERSON
+
+                //TO SHOW THE OUTPUT OF THE TIP PER PERSON.
+                listBox1.Items.Add(tipPerPerson.ToString("C"));
+
+                //TO SHOW THE OUTPUT OF THE BILL PER PERSON.
+                listBox2.Items.Add(billPerPerson.ToString("C"));
+            }
+            catch(MyException ex4)
+            {
+                //CALLING OF THE FUNCTION unexpectedError
+                ex4.UnexpectedError();
+
+            }                    
+
+        }
+
+      
+    }
+}
